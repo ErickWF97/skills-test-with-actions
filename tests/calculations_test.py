0449f3430b795a04965c2f4a390d94935522668b
ErickWF97 <87678407+ErickWF97@users.noreply.github.com>
2026-08-15 03:44:21 +0000

    simples commit para aprendizado

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -58,7 +58,7 @@ def test_get_nth_fibonacci_one():
     assert result == 1
 
 
-def test_get_nth_fibonacci_ten():
+gitdef test_get_nth_fibonacci_ten():
     """Test with n=10."""
     # Arrange
     n = 10
@@ -66,4 +66,4 @@ def test_get_nth_fibonacci_ten():
     result = get_nth_fibonacci(n)
 
     # Assert
-    assert result == 89
+    assert result == 55
